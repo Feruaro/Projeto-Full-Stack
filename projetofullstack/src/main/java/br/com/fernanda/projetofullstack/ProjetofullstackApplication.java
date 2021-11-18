c8697dc1e4e30bf3fe6cfabadc0daa9af724c17a
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 18:06:21 -0300

    instanciando objs e populando o DB - table Categoria

--- projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
@@ -1,13 +1,31 @@
 package br.com.fernanda.projetofullstack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetofullstackApplication {
+import br.com.fernanda.projetofullstack.domains.Categoria;
+import br.com.fernanda.projetofullstack.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetofullstackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository cat_repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofullstackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		cat_repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
