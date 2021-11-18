fc5155364d608838d6986f05bbe24c14aabc0971
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 14:09:15 -0300

    add dir do projeto

---
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.projetofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofullstackApplication.class, args);
+	}
+
+}
---
+++ projetofullstack/src/test/java/br/com/fernanda/projetofullstack/ProjetofullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernanda.projetofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
