e071adfba955406f7fa1b53db054119d376f8dc2
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 14:26:55 -0300

    Testando classe Categoria

--- projetofullstack/src/main/java/br/com/fernanda/projetofullstack/resources/CategoriaResource.java
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package br.com.fernanda.projetofullstack.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fernanda.projetofullstack.domains.Categoria;
 
 
 
@@ -11,7 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@GetMapping
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> listaCat = new ArrayList<>();
+		listaCat.add(cat1);
+		listaCat.add(cat2);
+		
+		return listaCat;
 	}
 }
