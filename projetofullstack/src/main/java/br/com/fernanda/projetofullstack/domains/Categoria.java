663915f6a85f6dce3d34f85c6d7de26c84f7feb1
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 14:22:33 -0300

    criada classe Categoria

---
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/domains/Categoria.java
@@ -0,0 +1,55 @@
+package br.com.fernanda.projetofullstack.domains;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+//Serializable = objetos da classe poderão ser convertidos para uma sequencia de bits, 
+//isso faz com  que os objetos possam ser gravados em arquivos, para trafegar em rede e etc;
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+	}
+	
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
