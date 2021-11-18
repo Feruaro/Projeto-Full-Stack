d1598f10d5bab2f77e9af5cc4468cf7d95923e51
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 18:41:52 -0300

    instanciando objs e populando DB

--- projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
@@ -8,14 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.fernanda.projetofullstack.domains.Categoria;
+import br.com.fernanda.projetofullstack.domains.Produto;
 import br.com.fernanda.projetofullstack.repositories.CategoriaRepository;
+import br.com.fernanda.projetofullstack.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetofullstackApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository cat_repo;
-	
+	@Autowired
+	private ProdutoRepository prod_repo;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofullstackApplication.class, args);
 	}
@@ -24,8 +27,19 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mousse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		cat_repo.saveAll(Arrays.asList(cat1, cat2));
+		prod_repo.saveAllAndFlush(Arrays.asList(p1, p2, p3));
 	}
 
 }
