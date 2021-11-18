3e647b124a69361dccec927588ccbfce5b214f42
Fernanda <fernandaruaro26@hotmail.com>
2021-11-18 18:41:12 -0300

    criada coleção e associação da Categoria com Produto

--- projetofullstack/src/main/java/br/com/fernanda/projetofullstack/domains/Categoria.java
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/domains/Categoria.java
@@ -1,12 +1,15 @@
 package br.com.fernanda.projetofullstack.domains;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -19,6 +22,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -41,6 +47,13 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -58,6 +71,8 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
