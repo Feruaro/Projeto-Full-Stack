de98ee557a5f13bb74a76107590d2140424f60e7
Fernanda <fernandaruaro26@hotmail.com>
2021-11-19 13:24:58 -0300

    instanciando, associando e populando o DB

--- projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
+++ projetofullstack/src/main/java/br/com/fernanda/projetofullstack/ProjetofullstackApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.fernanda.projetofullstack.domains.Categoria;
+import br.com.fernanda.projetofullstack.domains.Cidade;
+import br.com.fernanda.projetofullstack.domains.Estado;
 import br.com.fernanda.projetofullstack.domains.Produto;
 import br.com.fernanda.projetofullstack.repositories.CategoriaRepository;
+import br.com.fernanda.projetofullstack.repositories.CidadeRepository;
+import br.com.fernanda.projetofullstack.repositories.EstadoRepository;
 import br.com.fernanda.projetofullstack.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,11 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 	private CategoriaRepository cat_repo;
 	@Autowired
 	private ProdutoRepository prod_repo;
+	@Autowired
+	private EstadoRepository est_repo;
+	@Autowired
+	private CidadeRepository cid_repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetofullstackApplication.class, args);
 	}
@@ -27,6 +36,7 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mousse", 80.00);
@@ -36,10 +46,26 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));	
 		
 		cat_repo.saveAll(Arrays.asList(cat1, cat2));
-		prod_repo.saveAllAndFlush(Arrays.asList(p1, p2, p3));
+		
+		prod_repo.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado e1 = new Estado(null, "Minas Gerais");
+		Estado e2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", e1);
+		Cidade c2 = new Cidade(null, "São Paulo", e2);
+		Cidade c3 = new Cidade(null, "Campinas", e2);
+		
+		e1.getCidades().addAll(Arrays.asList(c1));
+		e2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		est_repo.saveAll(Arrays.asList(e1, e2));
+		
+		cid_repo.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
